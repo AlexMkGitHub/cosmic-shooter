1abc9d841cd2906d7658268bc221d25b4e12feb3
alexm <mkalex77@gmail.com>
2022-11-28 22:35:44 +0700

    коррекция

---
+++ desktop/src/dev/team/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package dev.team.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import dev.team.CosmicGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1280;
+        config.height = 720;
+		new LwjglApplication(new CosmicGame(), config);
+	}
+}
